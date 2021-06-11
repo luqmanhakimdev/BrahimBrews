373b5a0198ba82cab693e3edda9f06bb6461a950
toroi05 <fsthnd@gmail.com>
2021-06-11 18:52:54 +0800

    add

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,18 +6,17 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
             <!-- <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <x-jet-welcome />
             </div> -->
-
          <table class="table">
             <thead>
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">Flavor</th>
-                    <th scope="col">Stock In</th>
-                    <th scope="col">Stock Out</th>
+                    <th scope="col"><div class="text-center">Stock left</div></th>
+                    <th scope="col"><div class="text-center">Action</div></th>
                 </tr>
             </thead>
             <tbody>
@@ -26,12 +25,63 @@
                 <tr>
                     <th scope="row">{{  $flavor->id }}</th>
                     <td>{{ $flavor->flavor_name }}</td>
-                    <td>{{ $flavor->stock_in }}</td>
-                    <td>{{ $flavor->stock_out }}</td>
+                    <td><div class="text-center">{{ ($flavor->stock_in)-($flavor->stock_out) }}</div></td>
+                    <td><div class="text-center"><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" >Update</button></div></td>
                 </tr>
             @endforeach
             </tbody>
             </table>
+
+
+        <!-- Button trigger modal -->
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Update stock</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+        @csrf
+          <div class="mb-3">
+            <label for="stock_in" class="col-form-label">Stock in:</label>
+            <input type="text" class="form-control" id="stock_in">
+          </div>
+          <div class="mb-3">
+            <label for="stock_out" class="col-form-label">Stock out:</label>
+            <input type="text" class="form-control" id="stock_out"></input>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-success">Update</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script>
+            var exampleModal = document.getElementById('exampleModal')
+            exampleModal.addEventListener('show.bs.modal', function (event) {
+            // Button that triggered the modal
+            var button = event.relatedTarget
+            // Extract info from data-bs-* attributes
+            var recipient = button.getAttribute('data-bs-whatever')
+            // If necessary, you could initiate an AJAX request here
+            // and then do the updating in a callback.
+            //
+            // Update the modal's content.
+            var modalTitle = exampleModal.querySelector('.modal-title')
+            var modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+            modalBodyInput.value = recipient
+            })
+
+</script>
 
         </div>
     </div>
